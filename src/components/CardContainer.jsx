bda66ef79e19f5db7155484e5ad1baddd36efbfb
Shanehortonm <shanehortonm@gmail.com>
2023-10-28 02:37:56 -0400

    changes to packages

---
+++ src/components/CardContainer.jsx
@@ -0,0 +1,109 @@
+import { useState, useEffect } from "react";
+import { NavLink } from "react-router-dom";
+import MomCheckingKid from "../imgs/momcheckingkid.jpg";
+import KidSurprised from "../imgs/kidsurprised.jpg";
+import FingerPaint from "../imgs/fingerprintpaint.jpg";
+
+export const CardContainer = () => {
+  const [visibleCard, setVisibleCard] = useState(null);
+
+  const handleScroll = () => {
+    const cardElements = document.querySelectorAll(".card");
+    if (cardElements) {
+      let anyCardVisible = false;
+
+      cardElements.forEach((element, index) => {
+        const rect = element.getBoundingClientRect();
+        const thresholdMin = 200;
+        const thresholdMax = 550;
+
+        const isInViewport =
+          rect.top < window.innerHeight - thresholdMax &&
+          rect.bottom >= thresholdMin;
+
+        if (isInViewport) {
+          setVisibleCard(index + 1);
+          anyCardVisible = true;
+        }
+      });
+
+      if (!anyCardVisible) {
+        setVisibleCard(null);
+      }
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+  return (
+    <div className="cta-cards-container">
+      <div className="card-component-container">
+        <div className={`card card-1 ${visibleCard === 1 ? "hovered" : ""}`}>
+          <div className="card-1-img">
+            <img
+              loading="lazy"
+              src={MomCheckingKid}
+              alt="mom checking temperature of child"
+            />
+          </div>
+          <div className="card-1 card-info">
+            <h2>Protecting Your Child's Wellness</h2>
+            <p>
+              Discover the critical importance of precise administration and
+              tracking, and how to safeguard your little one from the potential
+              dangers of medication mishaps.
+            </p>
+            <button className="cta-btn-home">
+              <NavLink to={"ChildrenHealth"}>Get Started Now!</NavLink>
+            </button>
+          </div>
+        </div>
+
+        <div className={`card card-2 ${visibleCard === 2 ? "hovered" : ""}`}>
+          <div className="card-2-img">
+            <img
+              loading="lazy"
+              src={KidSurprised}
+              alt="mom checking temperature of child"
+            />
+          </div>
+          <div className="card-2 card-info">
+            <h2>Hear What Our Users Say</h2>
+            <p>
+              Discover what parents and caregivers have to say about their
+              experience with our app. We value their feedback and are proud to
+              share their stories of successful health management with you.
+            </p>
+            <button className="cta-btn-home">
+              <NavLink to={"Reviews"}>Read Reviews</NavLink>
+            </button>
+          </div>
+        </div>
+        <div className={`card card-3 ${visibleCard === 3 ? "hovered" : ""}`}>
+          <div className="card-3-img">
+            <img
+              loading="lazy"
+              src={FingerPaint}
+              alt="kid holding book, looking surprised"
+            />
+          </div>
+          <div className="card-3 card-info">
+            <h2>Discover Our Journey</h2>
+            <p>
+              Take a step back in time and explore the story behind our mission.
+              Learn how our passion for children's health led to the creation of
+              our app and the incredible journey that brought us here.
+            </p>
+            <button className="cta-btn-home">
+              <NavLink to={"Founder"}>Explore Our Story</NavLink>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ src/layouts/RootLayout.jsx
@@ -0,0 +1,106 @@
+import React, { useState } from "react";
+import { NavLink, Outlet } from "react-router-dom";
+import Headroom from "react-headroom";
+
+// Components
+import { ScrollToTop } from "../components/ScrollToTop";
+import { SubscribeBtn } from "../components/SubscribeBtn";
+import { GooglePlayBtn } from "../components/GooglePlayBtn";
+import { AppStoreBtn } from "../components/AppStoreBtn";
+import { SocialMediaIcons } from "../components/SocialMediaIcons";
+
+export const RootLayout = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <div className="root-layout">
+      <Headroom disableInlineStyles upTolerance={50} downTolerance={20}>
+        <header>
+          <nav>
+            <div className="nav-header">
+              <h1 className="logo">
+                <NavLink to={"/"}>OnCure</NavLink>
+              </h1>
+              <div
+                className={`menu-icon ${isMenuOpen ? "open" : ""}`}
+                onClick={toggleMenu}
+              >
+                <div className="bar"></div>
+                <div className="bar"></div>
+                <div className="bar"></div>
+              </div>
+            </div>
+            <ul className={`navlinks ${isMenuOpen ? "open" : ""}`}>
+              <NavLink to={"/"} onClick={toggleMenu}>
+                Home
+              </NavLink>
+              <NavLink to={"ChildrenHealth"} onClick={toggleMenu}>
+                Children's Health
+              </NavLink>
+              <NavLink to={"AppFeatures"} onClick={toggleMenu}>
+                Explore Our App
+              </NavLink>
+              <NavLink to={"Founder"} onClick={toggleMenu}>
+                Our Story
+              </NavLink>
+              <NavLink to={"Reviews"} onClick={toggleMenu}>
+                Reviews
+              </NavLink>
+              <NavLink to={"FaqLayout"} onClick={toggleMenu}>
+                Contact Us
+              </NavLink>
+            </ul>
+          </nav>
+        </header>
+      </Headroom>
+      <main>
+        <ScrollToTop />
+        <Outlet />
+      </main>
+
+      <footer>
+        <nav>
+          <h1 className="logo">
+            <NavLink to={"/"}>OnCure</NavLink>
+          </h1>
+          <div className="quick-links">
+            <NavLink to={"/"}>Home</NavLink>
+            <NavLink to={"ChildrenHealth"}>Children's Health</NavLink>
+            <NavLink to={"AppFeatures"}>Explore Our App</NavLink>
+            <NavLink to={"Founder"}>Our Story</NavLink>
+            <NavLink to={"Reviews"}>Reviews</NavLink>
+            <NavLink to={"FaqLayout"}>Contact Us</NavLink>
+          </div>
+          <div className="subscribe-btn-container">
+            <h2>Get All The Latest News!</h2>
+            <div>
+              <SubscribeBtn />
+            </div>
+          </div>
+          <div className="download-btn-container">
+            <GooglePlayBtn />
+            <AppStoreBtn />
+          </div>
+          <span className="footer-bar"></span>
+          <div className="copyright">
+            <p>
+              &#169; Copyright 2023 Myrtus Enterprises LLC. All rights reserved.
+            </p>
+            <div className="terms-privacy">
+              <NavLink to={"Terms"}>Terms & Conditions </NavLink>
+              <span>|</span>
+              <NavLink to={"Privacy"}> Privacy Policy</NavLink>
+            </div>
+          </div>
+          <div className="social-icons">
+            <SocialMediaIcons />
+          </div>
+        </nav>
+      </footer>
+    </div>
+  );
+};
