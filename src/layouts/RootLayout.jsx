0102c5d5820a127408537e9ab66626617f4086ed
Shanehortonm <shanehortonm@gmail.com>
2023-10-29 02:10:59 -0400

    work on contact section, work on founder picture responsivness

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -18,7 +18,7 @@ export const RootLayout = () => {
 
   return (
     <div className="root-layout">
-      <Headroom disableInlineStyles upTolerance={50} downTolerance={20}>
+      <Headroom disableInlineStyles upTolerance={30} downTolerance={20}>
         <header>
           <nav>
             <div className="nav-header">
