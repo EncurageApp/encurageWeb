ba28c315fad17570520849a1852c2159e0721a64
Shanehortonm <shanehortonm@gmail.com>
2023-11-01 08:20:15 -0400

    changes to founder

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// CSS
+import "./css/Founder.css";
+import "./css/Root.css";
+import "./css/NavBars.css";
+import "./css/Buttons.css";
+import "./css/Home.css";
+
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
