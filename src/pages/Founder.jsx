e454610d1121e633143b84cfffe531118f4b7980
Shanehortonm <shanehortonm@gmail.com>
2023-11-01 04:42:07 -0400

    changes to Title, Work on Founders page

--- src/pages/Founder.jsx
+++ src/pages/Founder.jsx
@@ -2,13 +2,24 @@ import React from "react";
 
 //imgs
 import Hadas from "../imgs/hadas.jpg";
+import DreamBig from "../imgs/dreambig.jpg";
 
 export const Founder = () => {
   return (
     <main>
+      <section className="founder-story-container">
+        <h2>Our Story</h2>
+        <div className="founder-img-container ">
+          <img
+            src={DreamBig}
+            alt="The Words Dream Big in White on a Black Background"
+          />
+          <p></p>
+        </div>
+      </section>
       <section className="founder-hero-container">
         <h2>Our Founder</h2>
-        <div className="founder-info">
+        <div className="founder-hero-info">
           <div className="founder-img-container">
             <img src={Hadas} alt="Founder of Company Hadas" />
           </div>
