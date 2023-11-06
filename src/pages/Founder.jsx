4185ed32618c1f9ef71e4d8b70aa087d4d5de69b
Shanehortonm <shanehortonm@gmail.com>
2023-11-06 00:28:20 -0500

    changes to founder page, built animateText and FounderAnimation components, need work on icons for animateText divs

--- src/pages/Founder.jsx
+++ src/pages/Founder.jsx
@@ -2,7 +2,8 @@ import React from "react";
 
 //imgs
 import Hadas from "../imgs/hadas.jpg";
-import { FounderAnimation } from "../components/FounderAnimation";
+
+import { AnimateText } from "../components/AnimateText";
 
 export const Founder = () => {
   return (
@@ -14,55 +15,64 @@ export const Founder = () => {
       </section>
       <section>
         <div className="f-story-container">
+          <div>
+            <div>
+              <h2>Our Founder</h2>
+              <p>
+                <span>Let</span> me introduce you to
+                <i className="name"> Hadas Kanner-Golan</i>, our exceptional
+                co-founder and CEO, leading the charge for OnCure.
+              </p>
+            </div>
+            <div className="f-founder-info">
+              <div className="f-founder-img-container">
+                <img src={Hadas} alt="Founder of Company Hadas" />
+              </div>
+            </div>
+          </div>
+
           <h2>Our Story</h2>
           <div>
-            <p>
-              <span>Let</span> me introduce you to{" "}
-              <i className="name">Hadas Kanner-Golan</i>, our exceptional
-              co-founder and CEO, leading the charge for OnCure.
-            </p>
-            <FounderAnimation />
+            <AnimateText>
+              <p>
+                <span>When</span> her daughter, Emily, was struck by strep
+                throat and a high fever, Hadas faced a daunting task: managing
+                the timing of her fever reducer doses. She was surprised to
+                discover the absence of a dedicated app to aid parents in
+                monitoring their children's medication. While numerous
+                medication reminders catered to adults, they fell short in
+                meeting the unique requirements of parents like Hadas.
+              </p>
+            </AnimateText>
           </div>
-          <p>
-            <span>When</span> her daughter, Emily, was struck by strep throat
-            and a high fever, Hadas faced a daunting task: managing the timing
-            of her fever reducer doses. She was surprised to discover the
-            absence of a dedicated app to aid parents in monitoring their
-            children's medication. While numerous medication reminders catered
-            to adults, they fell short in meeting the unique requirements of
-            parents like Hadas.
-          </p>
-          <p>
-            <span>Her</span> response was to dive into in-depth research,
-            probing the issue of medication errors within households. Through
-            this exploration, she unveiled the vastness of the problem and the
-            particular challenges it presented. This was the pivotal moment that
-            birthed the idea of OnCure. Hadas formed a partnership with a
-            board-certified pediatrician and an enthusiastic team of developers
-            to craft a secure, user-friendly solution for parents and
-            caregivers.
-          </p>
-        </div>
-      </section>
-      <section className="f-founder-container">
-        <h2>Our Founder</h2>
-        <div className="f-founder-info">
-          <div className="f-founder-img-container">
-            <img src={Hadas} alt="Founder of Company Hadas" />
-          </div>
+
           <div>
-            <h3>Hadas</h3>
-            <p>
-              Our Story is a tribute to Hadas Kanner-Golan, the driving force
-              behind the well-being and happiness of families.
-            </p>
-            <p>
-              Every day, we are grateful for Hadas's unwavering commitment to
-              empowering parents and ensuring the well-being of their children.
-              With a commitment to innovation, we have exciting plans for
-              OnCure, so be sure to subscribe here to stay updated on our latest
-              developments.
-            </p>
+            <AnimateText>
+              <p>
+                <span>Her</span> response was to dive into in-depth research,
+                probing the issue of medication errors within households.
+                Through this exploration, she unveiled the vastness of the
+                problem and the particular challenges it presented. This was the
+                pivotal moment that birthed the idea of OnCure. Hadas formed a
+                partnership with a board-certified pediatrician and an
+                enthusiastic team of developers to craft a secure, user-friendly
+                solution for parents and caregivers.
+              </p>
+            </AnimateText>
+          </div>
+
+          <div>
+            <AnimateText>
+              <p>
+                <span>Our </span>Story is a tribute to Hadas Kanner-Golan, the
+                driving force behind the well-being and happiness of families.
+                Every day, we are grateful for Hadas's unwavering commitment to
+                empowering parents and ensuring the well-being of their
+                children. With a commitment to innovation, we have exciting
+                plans for OnCure, so be sure to subscribe here to stay updated
+                on our latest developments.
+              </p>
+            </AnimateText>
           </div>
         </div>
       </section>
