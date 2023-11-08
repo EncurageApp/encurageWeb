11d9ee2d555a46a9d2597ca44da1c62eebf9971e
Shanehortonm <shanehortonm@gmail.com>
2023-11-08 05:23:16 -0500

    Changes

--- src/pages/AppFeatures.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const AppFeatures = () => {
-  return <div>AppFeatures</div>;
-};
--- src/pages/ChildrenHealth.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const ChildrenHealth = () => {
-  return <div>ChildrenHealth</div>;
-};
