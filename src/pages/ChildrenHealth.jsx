662a0a40dcfb5de03385f7a978c6d1b1b0763626
Shanehortonm <shanehortonm@gmail.com>
2023-11-16 21:45:41 -0500

    work with parallaz scrolling

--- src/pages/ChildrenHealth.jsx
+++ src/pages/ChildrenHealth.jsx
@@ -1,17 +1,20 @@
 import React from "react";
-import WashHands from "../imgs/kidwashinghand.jpg";
-import { HealthSlider } from "../components/HealthSlilder";
+import SilverLines from "../imgs/colorsmoke2.jpg";
+import { HealthSlider } from "../components/HealthSlider";
+import { Parallax } from "react-parallax";
 
 export const ChildrenHealth = () => {
   return (
     <main className="health-page">
       <section className="ch-hero">
-        <div>
-          <img
-            src={WashHands}
-            alt="father and daughter washing hands in kitchen"
-          />
-        </div>
+        <Parallax strength={500} bgImage={SilverLines}>
+          <div className="ch-intro">
+            <div className="ch-intro-text">
+              <h2>Navigating the Numbers,</h2>
+              <p>Exploring Children's Medication Errors</p>
+            </div>
+          </div>
+        </Parallax>
       </section>
 
       <section className="ch-slider-container">
