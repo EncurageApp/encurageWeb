b6ff917c4806a1433c761a1b643cefc771085575
Shanehortonm <shanehortonm@gmail.com>
2023-11-20 13:13:38 -0500

    work on children stats and hero

--- src/components/ChildrenStats.jsx
+++ src/components/ChildrenStats.jsx
@@ -1,16 +1,23 @@
 import React from "react";
 import AnimatedNumbers from "react-animated-numbers";
 
-export const ChildrenStats = () => {
+export const ChildrenStats = ({ animateNumber }) => {
   return (
     <div className="stats">
       <AnimatedNumbers
         includeComma
-        animateToNumber="200000"
-        fontStyle={{ fontSize: "5rem", color: "#008080" }}
-        configs={[{ mass: 1, tension: 220, friction: 100 }]}
+        animateToNumber={animateNumber}
+        fontStyle={{ color: "white" }}
+        configs={[
+          { mass: 1, tension: 220, friction: 100 },
+          { mass: 1, tension: 180, friction: 130 },
+          { mass: 1, tension: 280, friction: 90 },
+          { mass: 1, tension: 180, friction: 135 },
+          { mass: 1, tension: 260, friction: 100 },
+          { mass: 1, tension: 210, friction: 180 },
+        ]}
       ></AnimatedNumbers>
-      <div>%</div>
+      <div>+</div>
     </div>
   );
 };
--- src/pages/ChildrenHealth.jsx
+++ src/pages/ChildrenHealth.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import SilverLines from "../imgs/colorsmoke2.jpg";
+import SilverLines from "../imgs/dadtwokids.jpg";
 import { HealthSlider } from "../components/HealthSlider";
 import { Parallax } from "react-parallax";
 import { ChildrenStats } from "../components/ChildrenStats";
@@ -12,19 +12,48 @@ export const ChildrenHealth = () => {
           <div className="ch-intro">
             <div className="ch-intro-text">
               <h2>Navigating the Numbers,</h2>
-              <p>Exploring Children's Medication Errors</p>
+              <p>
+                Exploring Children's Medication Errors and How We Are Preventing
+                Them!
+              </p>
             </div>
           </div>
         </Parallax>
       </section>
 
       <section className="ch-stats-container">
-        <ChildrenStats />
+        <div className="ch-stats-card">
+          <div className="ch-card-icon"></div>
+          <div className="ch-card-content">
+            <ChildrenStats animateNumber={300000} />
+            <p>
+              incidents happen every year, according to the Poison Controls
+              Centers in the United States. 90% of these medication errors occur
+              at Home
+            </p>
+          </div>
+        </div>
+        <div className="ch-stats-card">
+          <div className="ch-card-icon"></div>
+          <div className="ch-card-content">
+            <ChildrenStats animateNumber={4000} />
+            <p>incidents have lead to children omitted to the ICU</p>
+          </div>
+        </div>
+        <div className="ch-stats-card">
+          <div className="ch-card-icon"></div>
+          <div className="ch-card-content">
+            <ChildrenStats animateNumber={20000} />
+            <p>Parents have taken the proactive step of downloading Encurage</p>
+          </div>
+        </div>
       </section>
 
-      <section className="ch-slider-container">
-        <HealthSlider />
-      </section>
+      {/* <section className="ch-slider-container">
+        <Parallax strength={500} bgImage={SilverLines}>
+          <HealthSlider />
+        </Parallax>
+      </section> */}
     </main>
   );
 };
