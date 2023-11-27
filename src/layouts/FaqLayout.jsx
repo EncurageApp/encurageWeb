afcf5330b1c2b77448032b07bb41fb016f167688
Shanehortonm <shanehortonm@gmail.com>
2023-11-27 13:57:35 -0500

    start of help center

--- src/layouts/FaqLayout.jsx
+++ src/layouts/FaqLayout.jsx
@@ -1,5 +1,22 @@
 import React from "react";
+import { NavLink, Outlet } from "react-router-dom";
 
 export const FaqLayout = () => {
-  return <div>FaqLayout</div>;
+  return (
+    <div className="help-layout">
+      <h2>Website Help</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, vel.
+      </p>
+
+      <nav>
+        <NavLink to="Contact">Contact Us</NavLink>
+        <NavLink to="Faq">View the Faq's</NavLink>
+        <NavLink to="Terms">Terms & Conditions</NavLink>
+        <NavLink to="Privacy">Privacy Policy</NavLink>
+      </nav>
+
+      <Outlet />
+    </div>
+  );
 };
